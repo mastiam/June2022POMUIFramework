0a0c6a10ad13fcdee1d48f6075a0ccc7178f5e5b
Mastan <mastan.tst@gmail.com>
2023-01-08 08:03:09 +0530

    local branch created and committed

--- src/test/java/com/qa/opencart/tests/GitCommitTest.java
+++ src/test/java/com/qa/opencart/tests/GitCommitTest.java
@@ -6,6 +6,7 @@ public class GitCommitTest {
 		// TODO Auto-generated method stub
 		System.out.println("This is just to test commits");
 		System.out.println("This is just to test commits user 2, user 2 chnages updated by user 1");
+		System.out.println("This is just to test commits user 1, localbranch create and check");
 	}
 
 }
