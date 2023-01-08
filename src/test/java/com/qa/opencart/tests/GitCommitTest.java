69ec8f95365b2820127e41930db5bc60c55d737b
mastiam <95519658+mastiam@users.noreply.github.com>
2023-01-08 07:24:36 +0530

    Update GitCommitTest.java

--- src/test/java/com/qa/opencart/tests/GitCommitTest.java
+++ src/test/java/com/qa/opencart/tests/GitCommitTest.java
@@ -5,6 +5,7 @@ public class GitCommitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is just to test commits");
+		System.out.println("This is just to test commits user 2");
 	}
 
 }
