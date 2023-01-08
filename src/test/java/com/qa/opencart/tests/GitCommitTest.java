67871a32684d01b3e7c969be212f3e027fca1f18
Mastan <mastan.tst@gmail.com>
2023-01-08 07:21:25 +0530

    This to test git flow commit 1

---
+++ src/test/java/com/qa/opencart/tests/GitCommitTest.java
@@ -0,0 +1,10 @@
+package com.qa.opencart.tests;
+
+public class GitCommitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is just to test commits");
+	}
+
+}
